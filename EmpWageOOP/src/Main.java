343d2854f57f48f1f086147c9311518a4268f81d
Sreenath285 <sreenath.285@gmail.com>
2022-02-25 14:17:22 +0530

    calculating employee till maximum hours or maximum days reached

---
+++ EmpWageOOP/src/Main.java
@@ -0,0 +1,49 @@
+public class Main {
+      /*
+        Program to compute employee wage
+     */
+
+    /*
+    init constants
+     */
+    private static final int isFullTime = 1;
+    private static final int isPartTime = 2;
+    private static final int wagePerHr = 20;
+    private static final int noOfWorkingDays = 20;
+    private static final int maxHrs = 100;
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Employee Wage Computation Program");
+
+        // init wagePerHr, fullDayHr, partTimeHr, dailyWage
+        // init empCheck and printing whether employee working full time or part time randomly
+        int empWorkHrs = 0, dailyWage = 0, monthlyWage = 0, totalWorkDays = 0, totalWorkHrs = 0;
+
+        // calculating wage till total working hours and days reached
+        while (totalWorkHrs <= maxHrs && totalWorkDays < noOfWorkingDays) {
+            totalWorkDays++;
+            double empCheck = Math.floor(Math.random() * 10) % 3;
+            switch ((int) empCheck) {
+                case isFullTime :
+                    System.out.println("Employee is present and working full time");
+                    empWorkHrs = 8;
+                    break;
+                case isPartTime :
+                    System.out.println("Employee is present and working part time");
+                    empWorkHrs = 4;
+                    break;
+                default :
+                    System.out.println("Employee is absent");
+                    empWorkHrs = 0;
+            }
+            totalWorkHrs += empWorkHrs;
+            dailyWage = wagePerHr * empWorkHrs;
+            monthlyWage += dailyWage;
+            System.out.println("Daily wage : " + dailyWage);
+        }
+        System.out.println("Total work hours : " + totalWorkHrs);
+        System.out.println("Total work days : " + totalWorkDays);
+        System.out.println("Monthly wage : " + monthlyWage);
+    }
+}
