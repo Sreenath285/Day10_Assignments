e1244dbccf515fafe1bab1917eab0b42def15552
Sreenath285 <sreenath.285@gmail.com>
2022-02-25 14:22:04 +0530

    computing multiple employee wages using classes and interface

---
+++ EmpWageOOP/src/EmpWageBuilder.java
@@ -0,0 +1,4 @@
+public interface EmpWageBuilder {
+
+    int calculateEmpWage();
+}
---
+++ EmpWageOOP/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+      /*
+        Program to compute employee wage
+     */
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Employee Wage Computation Program\n");
+
+        Employee actEmp = new Employee("ACT", 10, 10, 50);
+        actEmp.calculateEmpWage();
+
+        Employee trendsEmp = new Employee("Trends", 20, 20, 100);
+        trendsEmp.calculateEmpWage();
+
+    }
+}
